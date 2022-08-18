7c5420857f3293fe5384aaa12deeb9bbf222cdc9
Samuel Pletain <samuel.pletain@gmail.com>
2022-08-17 19:47:02 -0600

    Project setup

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/FEM-static-job-listings',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `import './src/assets/scss/_variables.scss';`
+      }
+    }
+  }
+})
